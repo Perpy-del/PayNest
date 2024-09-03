2d9ac7842131483c2846d6eeb2af1ea58057b10b
Perpy-del <perpetualmeninwa@gmail.com>
2024-09-03 14:54:49 +0100

    fix: :bug: clear comments

--- config/env.ts
+++ config/env.ts
@@ -1,28 +1,3 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
-// import { development } from './development.js';
-// import { production } from './production.js';
-// import { staging } from './staging.js';
-
-// const environment = process.env.NODE_ENV;
-
-// let config: Record<string, any>;
-
-// if (!environment) throw new Error('No environment setup');
-
-// // Logging to the console to indicate the environment
-// console.log(`server setup to ${environment}!!!👨‍💻`);
-
-// if (environment.trim() === 'development') {
-//   config = { ...development };
-// } else if (environment.trim() === 'staging') {
-//   config = { ...staging };
-// } else {
-//   config = { ...production };
-// }
-
-// export { config };
-
 import 'dotenv/config';
 
 export default {
