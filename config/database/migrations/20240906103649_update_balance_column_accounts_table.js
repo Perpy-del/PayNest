bf9af3d3e2d8825a09f7716da77be3becb1c7449
Perpy-del <perpetualmeninwa@gmail.com>
2024-09-06 11:54:02 +0100

    chore: :ambulance: update migrations using the cli

---
+++ config/database/migrations/20240906103649_update_balance_column_accounts_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  return knex.schema.table('accounts', table => {
+    table.decimal('balance', 15, 2).notNullable().defaultTo(0.0).alter();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema.table('accounts', table => {
+    table.dropColumn('is_default');
+  });
+}
