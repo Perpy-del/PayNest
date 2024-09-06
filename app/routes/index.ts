2b66dd4c9af100aa2e9e8150489e30a69150cc98
Perpy-del <perpetualmeninwa@gmail.com>
2024-09-06 09:06:23 +0100

    feat: :sparkles: add an endpoint to create an account and get all accounts for a user

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRouter from './auth.route.js';
 import userRouter from './user.route.js';
+import accountRouter from './account.route.js';
 
 const apiRouter = express.Router();
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
+apiRouter.use('/accounts', accountRouter);
 
 export default apiRouter;
