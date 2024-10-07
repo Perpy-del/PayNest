264eb084b8db1ff6d0f07238f33b6f2393765957
Perpy-del <perpetualmeninwa@gmail.com>
2024-10-07 08:19:14 +0100

    fix: :art: fix issue with secret

--- config/database/knexfile.js
+++ config/database/knexfile.js
@@ -7,11 +7,11 @@ dotenv.config();
  */
 
 const config = {
-  client: 'postgresql',
+  client: process.env.DB_CLIENT,
   connection: {
-    database: 'postgres',
-    user: 'postgres',
-    password: 'Secret321@',
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
   pool: {
     min: 2,
